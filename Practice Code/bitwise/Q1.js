0ae0f4cf0da77e842597fe117f96918acd286970
Mohit Soni <sonimohit481@gmail.com>
2022-09-19 12:12:03 +0530

    1

---
+++ Practice Code/bitwise/Q1.js
@@ -0,0 +1,7 @@
+//  basic bitwis operator;
+let a = 4;
+let b = 6;
+console.log("OR Operator", a || b)
+console.log("And Operator", a && b)
+console.log("Negative opertor", ~a)
+console.log("XOR operator", a ^ b)

7604671b06135881ac340e0975fff7b53f4311c5
Mohit Soni <sonimohit481@gmail.com>
2022-09-19 12:25:12 +0530

    2

---
+++ Practice Code/bitwise/shiftyoperator.js
@@ -0,0 +1,6 @@
+console.log("shifting 5 towrds left by to bit and the oupput will be   ", 5 << 2)
+console.log("shifting 5 towrds right by to bit and the oupput will be   ", 5 >> 2)
+
+//  in case of positiv no the padding bit|| the adding bit that woulded be shifting is 0
+//  but in case of -ve no it is compiler dependendent
+console.log(-5 >> 2)
---
+++ inc_dec.js
@@ -0,0 +1,9 @@
+let i = 7;
+console.log(i++)
+// first 7 then 8
+console.log(++i)
+// 8 the first ine then 9
+console.log(i--)
+// last 9 the dec so 8
+console.log(--i)
+// last 8 then first dec 7

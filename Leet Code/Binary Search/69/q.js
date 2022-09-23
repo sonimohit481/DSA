2d9feb247eb64639f815e815f96876fff729e426
Mohit Soni <sonimohit481@gmail.com>
2022-09-23 19:11:52 +0530

    8

--- Leet Code/Binary Search/69/q.js
+++ Leet Code/Binary Search/69/q.js
@@ -1,12 +1,47 @@
 function runProgram(input) {
+    input
     mySqrt(input);
 }
 var mySqrt = function (x) {
-    // second approach
-    return Math.floor(Math.sqrt(x));
+    // first approach
+
+    // return Math.floor(Math.sqrt(x));
+    let i = 0, j = x;
+    let ans = -1;
+    while (i < j) {
+        i
+
+        j
+        let mid = Math.floor(i + (j - i) / 2);
+        mid
+        let s = mid * mid;
+        s
+        if (s === x) {
+            mid
+            return mid;
+        }
+        else if (s < x) {
+            ans
+            s
+            mid
+            ans = mid;
+            i = mid + 1;
+        }
+        else {
+            mid
+            j = mid - 1;
+        }
+
+    }
+    ans
+    return ans;
+    // -----therd
+    // if (x === 0 || x === 1) return x;
+    // let counter = 2;
+    // while (true) { if (counter * counter > x) break; counter++ } return counter - 1;
 };
 if (process.env.USERNAME === "Mohit Soni") {
-    runProgram(`8`);
+    runProgram(`9`);
 } else {
     process.stdin.resume();
     process.stdin.setEncoding("ascii");
